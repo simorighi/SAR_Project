b54fb5dbaaf1b62936d0fa3def50772b0bc61992
simoneRighi <simone.righi@marconirovereto.it>
2025-11-12 11:22:42 +0100

    settingPrj

---
+++ SARprj/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/navbar/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  return (
+    <>
+    <header>
+      <Navbar></Navbar>
+    </header>
+    </>
+  )
+}
+
+export default App
