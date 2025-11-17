7047097edd8793e3320d1be8e8faf30c0f792a66
simoneRighi <simone.righi@marconirovereto.it>
2025-11-17 16:50:18 +0100

    yess

--- SARprj/src/components/navigationButton/navigationButton.tsx
+++ SARprj/src/components/navigationButton/navigationButton.tsx
@@ -2,16 +2,23 @@ import "./navigationButton.css";
 
 function NavigationButton() {
   return (
-    <div className="container border rounded-4 mt-5 bacColor">
-      <div className="row">
-        <div className="col-12 d-flex justify-content-center gap-3 my-5 ">
-          <button>
-            <p>Learn More</p>
-            <span></span>
-          </button>
+    <>
+      <div className="container border rounded-4 mt-5 bacColor hero-container position-relative">
+      <div className="cerchio-1"></div>
+      <div className="cerchio-2"></div>
+      <div className="cerchio-3"></div>
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center gap-3 my-5 ">
+            <button>
+              <p>Learn More</p>
+              <span></span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      
+    </>
   );
 }
 
