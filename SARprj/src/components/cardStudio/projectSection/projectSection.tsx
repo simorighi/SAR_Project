c002d353543675c031afa70bf9b60a02cff21286
simoneRighi <simone.righi@marconirovereto.it>
2025-11-24 16:52:14 +0100

    tictac

--- SARprj/src/components/cardStudio/projectSection/projectSection.tsx
+++ SARprj/src/components/cardStudio/projectSection/projectSection.tsx
@@ -6,11 +6,7 @@ import img1 from "../../../assets/houseImg.jpg";
 import img2 from "../../../assets/houseImg.jpg";
 
 function ProjectSection() {
-
-  const imgProjects =[
-    img0, img1, img2
-  ]
-
+  const imgProjects = [img0, img1, img2];
 
   const projects = [
     {
@@ -34,20 +30,46 @@ function ProjectSection() {
       ),
     },
 
-
     {
       imgSrc: imgProjects[1],
       titleCard: "Progetto 2",
       textCard: "Descrizione del progetto 2",
-      textButton: "Scopri di più",
+      textButton: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="60"
+          height="26"
+          fill="currentColor"
+          className="bi bi-arrow-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
+      ),
     },
-
 
     {
       imgSrc: imgProjects[2],
       titleCard: "Progetto 3",
       textCard: "Descrizione del progetto 3",
-      textButton: "Scopri di più",
+      textButton: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="60"
+          height="26"
+          fill="currentColor"
+          className="bi bi-arrow-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
+      ),
     },
   ];
 
