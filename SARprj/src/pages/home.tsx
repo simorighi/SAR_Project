b5b24fee5c9f10585e249e9c11bbb72a56fb8cab
simoneRighi <simone.righi@marconirovereto.it>
2026-04-15 10:14:29 +0200

    nuovo inizio

--- SARprj/src/pages/home.tsx
+++ SARprj/src/pages/home.tsx
@@ -1,13 +1,9 @@
-import CardStudio from "../components/cardStudio/cardStudio";
-import CardTitle from "../components/cardTitle/cardTitle";
-import CardNavigation from "../components/cardNavigation/cardNavigation";
+
 
 function Home() {
   return (
     <>
-      <CardTitle />
-      <CardNavigation/>
-      <CardStudio />
+     
       
     </>
   );
